e230e32545031d562c7648b3d35394856f5c611f
Guilherme Sampaio <guilhermesampaio@localhost.localdomain>
2019-03-27 07:29:00 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,8 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            I'll be changed
+            Guilherme Sampaio
           </a>
         </header>
       </div>
